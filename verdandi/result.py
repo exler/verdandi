7bceca73fb7208de2d522bf143948e5b75511ca1
Kamil Marut <kamil.marut@pm.me>
2021-10-06 09:06:55 +0200

    feat: Get memory usage

--- verdandi/result.py
+++ verdandi/result.py
@@ -5,3 +5,6 @@ from dataclasses import dataclass
 class BenchmarkResult:
     name: str
     duration_sec: float = 0.0
+
+    # Memory allocated in bytes
+    memory_diff: float = 0.0
--- verdandi/runner.py
+++ verdandi/runner.py
@@ -1,6 +1,7 @@
+import tracemalloc
 from statistics import mean
 from time import perf_counter
-from typing import Any, Callable, List
+from typing import Any, Callable, Dict, List
 
 from verdandi.benchmark import Benchmark
 from verdandi.result import BenchmarkResult
@@ -11,18 +12,26 @@ class BenchmarkRunner:
 
     def measure(self, func: Callable[..., Any]) -> BenchmarkResult:
         start_time = perf_counter()
+        start_snapshot = tracemalloc.take_snapshot()
 
         func()
 
+        stop_snapshot = tracemalloc.take_snapshot()
         stop_time = perf_counter()
+
         time_taken = stop_time - start_time
-        return time_taken
+        memory_diff = stop_snapshot.compare_to(start_snapshot, "lineno")
+
+        stats = {"time": time_taken, "memory": memory_diff}
+
+        return stats
 
     def run(self, benchmark: Benchmark, iterations: int = 10) -> None:
         benchmark = benchmark()
         methods = benchmark.collect_bench_methods()
 
-        times: List[float] = []
+        stats: List[Dict[str, Any]] = []
+        tracemalloc.start()
 
         benchmark.setUpClass()
 
@@ -31,8 +40,8 @@ class BenchmarkRunner:
             for _ in range(iterations):
                 benchmark.setUpIter()
 
-                time_taken = self.measure(method)
-                times.append(time_taken)
+                iter_stats = self.measure(method)
+                stats.append(iter_stats)
 
                 benchmark.tearDownIter()
 
@@ -40,4 +49,9 @@ class BenchmarkRunner:
 
         benchmark.tearDownClass()
 
-        return BenchmarkResult(name="test", duration_sec=mean(times))
+        return BenchmarkResult(
+            name="test",
+            duration_sec=mean([s["time"] for s in stats]),
+            # StatisticDiff is sorted from biggest to the smallest
+            memory_diff=mean([s["memory"][0].size_diff for s in stats]),
+        )
